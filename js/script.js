5e9214a82bdee896c0e6798f38c921e1deaa1135
Kostia <hks1153@gmail.com>
2018-08-31 21:09:43 +0300

    nav end

--- js/script.js
+++ js/script.js
@@ -3,9 +3,39 @@ function mobiopenclose(){document.getElementById("mobileMenujs").onclick = funct
    document.getElementsByClassName('mobile_nav')[0].style.display="block";}
    else {document.getElementsByClassName('mobile_nav')[0].style.display="none";}
 }
-}
+} // открытие/закрытие бургера это просто =)))
 
- 
+
+window.onscroll = function sc() {
+     var nav = document.getElementById('nav');
+
+     if(window.pageYOffset > 100){
+
+     nav.style.position = "fixed";
+     nav.style.top = 0;
+     nav.style.width = '80%';
+          
+     }else{
+      // nav.style.position = "absolute";
+     nav.style.position = 'relative'; //fixed
+     nav.style.top = 100;
+     nav.style.width = '100%';
+    }
+} 
+
+/*        document.getElementsByClassName('nav')[0].style.position = "fixed";
+          document.getElementsByClassName('nav')[0].style.top = 0;
+          document.getElementsByClassName('nav')[0].style. = 1280;
+
+          }else{
+            // nav.style.position = "absolute";
+            document.getElementsByClassName('nav')[0].style.position = 'relative'; //fixed
+            document.getElementsByClassName('nav')[0].style.top = 100;
+          }
+       }
+
+
+
 /*
 window.onload = function(){document.getElementById('mobileMenujs').onmouseover=function(event) {
     var target = event.target; // где был клик?
